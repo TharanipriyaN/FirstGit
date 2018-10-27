f9d57100712e7317336d8577b04d24d5716b5e0e
tharanipriyan <tharanipriya.eni@gmail.com>
2018-10-27 03:18:07 -0700

    Enhanced for loop done

--- src/git/org/FirstGitProject.java
+++ src/git/org/FirstGitProject.java
@@ -2,8 +2,13 @@ package git.org;
 
 public class FirstGitProject {
 	public static void main(String[] args) {
+		int a[] = {10,20,30};
 		for(int i=0;i<=10;i++) {
 			System.out.println(i);
+		}
+		for(int j:a)
+		{
+			System.out.println(j);
 		}
 	}
 
